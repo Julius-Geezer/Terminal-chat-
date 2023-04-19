b0a16faf8bc56ea05fa8c97f8ccc2e1a028b9b96
Julius-Geezer <willstephenson66@gmail.com>
2023-04-19 12:28:32 +1000

    Added comments and ability to input ip address on server script

--- serverterm.py
+++ serverterm.py
@@ -1,22 +1,32 @@
 import socket
 import threading
 import rsa 
+import os 
 
-host = '10.0.0.100' 
+ip_address = input('Enter IP address: ') 
+
+# Initializing server
+host = ip_address 
 port = 9999 
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 server.bind((host, port)) 
 server.listen() 
+print('[*] Server is online') 
 
+
+# Storing clients and their names 
 clients = [] 
 names = [] 
 
+
+# Sending messages to all clients connected to the server 
 def broadcast_message(message):
     for client in clients: 
         client.send(message) 
 
 
+# Saving messages from clients then sending them to other clients using previous function 
+# Handling clients leaving the server
 def handle_clients(client): 
     while True:
         try:
@@ -32,18 +42,21 @@ def handle_clients(client):
             break 
 
 
+# Handling server side information
+# Sending connection details to clients 
+# Initializing thread 
 def server_receive():
     while True:
         client, address = server.accept()
-        print('Connected with {}'.format(str(address))) 
+        print('[*] Connected with {}'.format(str(address))) 
         client.send('NAME'.encode('ascii'))
         name = client.recv(1024).decode('ascii') 
         names.append(name) 
         clients.append(client)
         
-        print('User {} has connected'.format(name)) 
+        print('[*] User {} has connected'.format(name)) 
         broadcast_message('{} has joined'.format(name).encode('ascii'))
-        client.send('Connection successful!'.encode('ascii'))
+        client.send('Connection successful'.encode('ascii'))
 
         thread = threading.Thread(target=handle_clients, args=(client,)) 
         thread.start()
@@ -51,5 +64,4 @@ def server_receive():
 
 if __name__ == '__main__':
     server_receive()
-    print('[*] Server is active') 
 
